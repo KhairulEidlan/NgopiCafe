dcccfa32e8fbebfeddc45908fafde64b7594d26f
Khairul Eidlan <khairuleidlan14@gmail.com>
2021-12-15 19:39:26 +0800

    update app category

--- app/src/main/java/com/example/ngopi/apps/AppCategoryActivity.java
+++ app/src/main/java/com/example/ngopi/apps/AppCategoryActivity.java
@@ -24,7 +24,7 @@ public class AppCategoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
+        setContentView(R.layout.activity_app_category);
 
         //hide status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
