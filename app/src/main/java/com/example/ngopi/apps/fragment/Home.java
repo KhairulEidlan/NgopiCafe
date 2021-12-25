3b28aa10482eb556f4809b0419587cad55040458
adamdanieal <adamdanieal98@gmail.com>
2021-12-26 07:45:50 +0800

    dashboard update

--- app/src/main/java/com/example/ngopi/apps/fragment/Home.java
+++ app/src/main/java/com/example/ngopi/apps/fragment/Home.java
@@ -1,7 +1,10 @@
 package com.example.ngopi.apps.fragment;
 
+import android.app.Dialog;
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,25 +12,62 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ngopi.R;
+import com.example.ngopi.admin.AdminCategory;
+import com.example.ngopi.apps.model.RvMenu;
 import com.example.ngopi.apps.rv.RvCatAdapter;
 import com.example.ngopi.apps.model.RvCat;
+import com.example.ngopi.apps.rv.RvMenuAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends Fragment {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     RecyclerView recyclerView;
+    ImageView imgView,btnAdd;
+    TextView menuTitle,menuPrice;
+
+
+    String username;
+    String link;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        //get pass data username
         Bundle bundle = this.getArguments();
-        String username = bundle.getString("username");
+        username = bundle.getString("username");
 
         recyclerView = view.findViewById(R.id.rcview);
+        imgView = view.findViewById(R.id.imgView);
+//        imgView1 = view.findViewById(R.id.imgView1);
+//        imgView2 = view.findViewById(R.id.imgView2);
+        menuTitle = view.findViewById(R.id.menuTitle);
+        menuPrice = view.findViewById(R.id.menuPrice);
+        btnAdd = view.findViewById(R.id.btnAdd);
+
+
+        categories();
+
+        showitem1();
+        return view;
+    }
+    public void categories(){
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false));
 
         // Item
@@ -40,6 +80,86 @@ public class Home extends Fragment {
 
         recyclerView.setAdapter(new RvCatAdapter(getContext(),username, item));
 
-        return view;
     }
+
+    public void showitem1(){
+        db.collection("Category")
+                .whereEqualTo("name","Coffee")
+                .get()
+                .addOnCompleteListener(taskCategory -> {
+                    if (taskCategory.isSuccessful()){
+                        for (QueryDocumentSnapshot documentCategory : taskCategory.getResult()){
+                            documentCategory.getReference().collection("Menu")
+                                    .whereEqualTo("menu_name","Caramel Latte")
+                                    .whereEqualTo("is_active",true)
+                                    .get()
+                                    .addOnCompleteListener(taskMenu -> {
+                                        if (taskMenu.isSuccessful()){
+                                            for (QueryDocumentSnapshot documentMenu : taskMenu.getResult()){
+                                                menuTitle.setText(documentMenu.getData().get("menu_name").toString());
+                                                menuPrice.setText("RM "+documentMenu.getData().get("menu_price").toString());
+                                                link = documentMenu.getData().get("menu_pic").toString();
+                                                Picasso.get().load(link).into(imgView);
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Dialog dialog = new Dialog(getActivity());
+                View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_menu,null);
+                dialog.setContentView(view);
+                dialog.getWindow().setBackgroundDrawable(ContextCompat.getDrawable(view.getContext(), R.drawable.dialog_background));
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
+                dialog.show();
+                dialog.setCancelable(true);
+
+                TextView lblItemMenu;
+                ImageView imageItemMenu;
+                Spinner spinnerMenuItem;
+                TextView lblItemMenuInfo;
+                Button btnConfirm;
+
+                String[] arraytype = {"Small","Regular"};
+                String type ;
+                double amount;
+
+                lblItemMenu = view.findViewById(R.id.lblItemMenu);
+                imageItemMenu = view.findViewById(R.id.imgItemMenu);
+                spinnerMenuItem = view.findViewById(R.id.spinnerMenuItem);
+                lblItemMenuInfo = view.findViewById(R.id.lblItemMenuInfo);
+                btnConfirm = view.findViewById(R.id.btnConfirm);
+
+                lblItemMenu.setText(menuTitle.getText());
+                lblItemMenuInfo.setText("*For regular type, item will added RM 1.00");
+                Picasso.get().load(link).into(imageItemMenu);
+
+
+                ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, arraytype);
+                dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerMenuItem.setAdapter(dataAdapter);
+                if (spinnerMenuItem.getSelectedItem().toString().equals("Regular")){
+                    amount = 1.00;
+                }
+
+                btnConfirm.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        Toast.makeText(getActivity(), spinnerMenuItem.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+                        //masuk db
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
 }
